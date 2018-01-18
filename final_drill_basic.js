ffed5d84fe6b2b065c3c527f1077860b4227ed7c
Dustin Vandehey <dustinvandehey@gmail.com>
2018-01-17 20:57:07 -0800

    Javascript changes to file
    Update to JavaScript final_drill_basic.js.
    This should have all of the functions.

--- final_drill_basic.js
+++ final_drill_basic.js
@@ -24,6 +24,10 @@ function getReceipt(){
 		console.log("size text1: "+text1);
 		console.log("subtotal:$"+runningTotal+".00");
 		getMeat(runningTotal,text1);
+		getCheese(runningTotal,text1);
+		getVeggies(runningTotal,text1);
+		getSauce(runningTotal,text1);
+		getCrust(runningTotal,text1)
 		};
 	function getMeat(runningTotal,text1){
 		var meatTotal=0;
@@ -49,5 +53,102 @@ function getReceipt(){
 	console.log("Purchase Total: "+"$"+runningTotal+".00");
 	document.getElementById("showText").innerHTML=text1;
 	document.getElementById("totalPrice").innerHTML="<h3>Total:<strong>$"+runningTotal+".00"+"</strong></h3>";
-	
 	};
+function getCheese(runningTotal,text1){
+		var cheeseTotal=0;
+		var selectedCheese=[];
+		var cheeseArray=document.getElementsByClassName("cheese");
+		for(var h=0; h<cheeseArray.length;h++){
+			if(cheeseArray[h].checked){
+				var selectedCheese=cheeseArray[h].value;
+				text1=text1+selectedCheese+"<br>";
+		}
+	}
+		if (selectedCheese==="Regular"){
+			cheeseTotal=0;
+		}else if(selectedCheese==="No Cheese"){
+			cheeseTotal=0;
+		}else if(selectedCheese==="Extra Cheese"){
+			cheeseTotal=3;
+		}
+		
+		runningTotal=cheeseTotal;
+		console.log(selectedCheese+"=$"+cheeseTotal+".00");
+		console.log("cheese text1: "+text1);
+		console.log("subtotal:$"+runningTotal+".00");
+};
+function getVeggies(runningTotal,text1){
+		var veggiesTotal=0;
+		var selectedVeggies=[];
+		var veggiesArray=document.getElementsByClassName("veggies");
+		for(var k=0; k<veggiesArray.length;k++){
+			if(veggiesArray[k].checked){
+				selectedVeggies.push(veggiesArray[k].value);
+				console.log("selected veggies item:("+veggiesArray[k].value+")");
+				text1=text1+veggiesArray[k].value+"<br>";
+			}
+		}
+	var veggiesCount=selectedVeggies.length;
+	if (veggiesCount>1){
+		veggiesTotal=(veggiesCount-1);
+	}else{
+		veggiesTotal=0;
+	}
+	runningTotal=(runningTotal+veggiesTotal);
+	console.log("total selected veggies items:"+veggiesCount);
+	console.log(veggiesCount+"veggies-1 free veggies="+"$"+veggiesTotal+".00");
+	console.log("veggies text1: "+text1);
+	console.log("Purchase Total: "+"$"+runningTotal+".00");
+	document.getElementById("showText").innerHTML=text1;
+	document.getElementById("totalPrice").innerHTML="<h3>Total:<strong>$"+runningTotal+".00"+"</strong></h3>";
+	};
+function getCrust(runningTotal,text1){
+		var crustTotal=0;
+		var selectedCrust=[];
+		var crustArray=document.getElementsByClassName("crust");
+		for(var m=0; m<cheeseArray.length;m++){
+			if(crustArray[m].checked){
+				var selectedCrust=crustArray[m].value;
+				text1=text1+selectedCrust+"<br>";
+		}
+	}
+		if (selectedCrust==="Plain Crust"){
+			crustTotal=0;
+		}else if(selectedCrust==="Garlic Butter Crust"){
+			crustTotal=0;
+		}else if(selectedCrust==="Cheese Stuffed Crust"){
+			crustTotal=3;
+		}else if(selectedCrust==="Spicy Crust"){
+			crustTotal=0;
+		}else if(selectedCrust==="Home Special Crust"){
+			crustTotal=0;
+		}
+		runningTotal=crustTotal;
+		console.log(selectedCrust+"=$"+crustTotal+".00");
+		console.log("crust text1: "+text1);
+		console.log("subtotal:$"+runningTotal+".00");
+};
+function getSauce(runningTotal,text1){
+		var SauceTotal=0;
+		var selectedSauce=[];
+		var sauceArray=document.getElementsByClassName("sauce");
+		for(var a=0; a<cheeseArray.length;a++){
+			if(sauceArray[a].checked){
+				var selectedSauce=SauceArray[a].value;
+				text1=text1+selectedCrust+"<br>";
+		}
+	}
+		if (selectedSauce==="Marinara Sauce"){
+			sauceTotal=0;
+		}else if(selectedSauce==="White Sauce"){
+			sauceTotal=0;
+		}else if(selectedSauce==="Barbecue Sauce"){
+			sauceTotal=0;
+		}else if(selectedSauce==="No Sauce"){
+			sauceTotal=0;
+		}
+		runningTotal=sauceTotal;
+		console.log(selectedSauce+"=$"+sauceTotal+".00");
+		console.log("sauce text1: "+text1);
+		console.log("subtotal:$"+runningTotal+".00");
+};
