b583099333edb53ae944a4643fc8744abe18a1b7
Dustin Vandehey <dustinvandehey@gmail.com>
2018-02-06 22:08:03 -0800

    JavaScript Edits

--- JavaScript/final_drill_basic.js
+++ JavaScript/final_drill_basic.js
@@ -48,8 +48,8 @@ function getReceipt(){
 	console.log(meatCount+"meat-1 free meat="+"$"+meatTotal+".00");
 	console.log("meat text1: "+text1);
 	console.log("Purchase Total: "+"$"+runningTotal+".00");
-	document.getElementById("showText").innerHTML=text1;
-	document.getElementById("totalPrice").innerHTML="<h3>Total:<strong>$"+runningTotal+".00"+"</strong></h3>";
+	//document.getElementById("showText").innerHTML=text1;
+	//document.getElementById("totalPrice").innerHTML="<h3>Total:<strong>$"+runningTotal+".00"+"</strong></h3>";
 	getCheese(runningTotal,text1);
 	};
 function getCheese(runningTotal,text1){
@@ -127,6 +127,8 @@ function getCrust(runningTotal,text1){
 		console.log(selectedCrust+"=$"+crustTotal+".00");
 		console.log("crust text1: "+text1);
 		console.log("subtotal:$"+runningTotal+".00");
+		document.getElementById("showText").innerHTML=text1;
+		document.getElementById("totalPrice").innerHTML="<h3>Total:<strong>$"+runningTotal+".00"+"</strong></h3>";
 		getSauce(runningTotal,text1)
 		};
 function getSauce(runningTotal,text1){
